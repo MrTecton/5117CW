17fdb8964d00f3c4a1e943047c9ec1e03d6b7306
Jake Harris <39124661+MrTecton@users.noreply.github.com>
2018-10-16 13:42:55 +0100

    Added Comments + Variable Counts

--- 5117COMP/src/CW/PDA.java
+++ 5117COMP/src/CW/PDA.java
@@ -5,26 +5,22 @@ import java.util.ArrayList;
 
 public class PDA {
 	
+	//Initiliation Array
 	static ArrayList<String> crimeDLA = new ArrayList<String>();
 	static String[] data = new String[15];
 	
+	//Buffered Reader
 	static BufferedReader br = null;
 	
+	//Buffered Reader String
 	static String brCDLA = "";
-	static String csvSplit = ",";
-	
-	static String C02 = "src/2018-02/2018-02-cheshire-stop-and-search.csv"; 
-	static String M02 = "src/2018-02/2018-02-merseyside-stop-and-search.csv";
-	static String C03 = "src/2018-03/2018-03-cheshire-stop-and-search.csv";
-	static String M03 = "src/2018-03/2018-03-merseyside-stop-and-search.csv";
-	static String C04 = "src/2018-04/2018-04-cheshire-stop-and-search.csv";
-	static String M04 = "src/2018-04/2018-04-merseyside-stop-and-search.csv";
-	
-	
-	
-	/* public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	} */
+	//String Variables
+	static String csvSplit = ",";
+
+	//Variable Counters
+	static int successful = 0;
+	static int partsuccessful = 0;
+	static int unsuccessful = 0;
 
 }
